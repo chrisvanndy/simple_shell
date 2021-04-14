c0b0670c5bd21b91666ffe84494232d9b81f8c32
chrisvanndy <chrisvannyc@gmail.com>
2021-04-14 02:42:09 +0000

    exit built in -- no null

--- main.c
+++ main.c
@@ -116,7 +116,7 @@ int compareStr(char **av, char *cmd, char **toks, int count)
 		free_toks(toks);
 		return (0);
 	}
-	if (_strcmp(toks[0], "exit\0") == 0)
+	if (_strcmp(toks[0], "exit") == 0)
 	{
 		free(cmd);
 		free_toks(toks);
